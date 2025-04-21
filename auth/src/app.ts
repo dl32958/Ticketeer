37675bf36e248d60e9a051fbaf1abe7272c085ae
Dong Lu <144203360+dl32958@users.noreply.github.com>
2025-04-20 21:49:12 -0700

    add common npm library

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,13 +2,12 @@ import express from 'express';
 import 'express-async-errors'; // for async error handling
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@dlticketeer/common';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.set('trust proxy', true); // make express trust the proxy (ingress-nginx)
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@dlticketeer/common';
 
 const router = express.Router();
 
